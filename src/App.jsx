98a22f3f1121f482cf0522838f8cf6d844698904
Onidaresam <onidaresam@yahoo.com>
2026-01-28 20:10:43 +0000

    Remove old technology folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Bands from './components/Band/Bands';
 import About from './components/Aboutus/About';
-import Technologies from './components/Technology/Technologies';
 //import Faqs from './components/FAQs/Faqs';
 function App() {
   return (
@@ -11,8 +10,7 @@ function App() {
         <Routes>
           <Route path="" element={<Bands />} />
           <Route path="/aboutus" element={<About />} />
-          <Route path="/technologies" element={<Technologies />} />
-        </Routes>
+          </Routes>
       </Router>
     </div>
   );
